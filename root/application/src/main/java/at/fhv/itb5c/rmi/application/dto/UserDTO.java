22883720df9cee1837e461cdc1d493155a5b4c69
DaGrisa <dgr7348@students.fhv.at>
2015-10-23 16:25:15 +0200

    merge with dto module
    Signed-off-by: DaGrisa <dgr7348@students.fhv.at>

--- root/application/src/main/java/at/fhv/itb5c/rmi/application/dto/UserDTO.java
+++ root/application/src/main/java/at/fhv/itb5c/rmi/application/dto/UserDTO.java
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 package at.fhv.itb5c.rmi.application.dto;
 
 public class UserDTO {
 
 }
+=======
+package at.fhv.itb5c.rmi.application.dto;
+
+import at.fhv.itb5c.rmi.dto.interfaces.IUser;
+
+public class UserDTO implements IUser {
+	public UserDTO(IUser user){
+		
+	}
+}
+>>>>>>> merge with dto module

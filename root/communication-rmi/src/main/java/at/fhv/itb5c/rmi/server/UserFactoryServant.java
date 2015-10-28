f14dae674db5e20428ad130bbbdd9292768e778a
DaGrisa <dgr7348@students.fhv.at>
2015-10-28 15:38:28 +0100

    rmi server bugfixes

--- root/communication-rmi/src/main/java/at/fhv/itb5c/rmi/server/UserFactoryServant.java
+++ root/communication-rmi/src/main/java/at/fhv/itb5c/rmi/server/UserFactoryServant.java
@@ -3,14 +3,12 @@ package at.fhv.itb5c.rmi.server;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 
 import at.fhv.itb5c.commons.dto.IUser;
 import at.fhv.itb5c.commons.dto.IUserFactory;
 import at.fhv.itb5c.commons.dto.rmi.IUserFactoryRMI;
 
-public class UserFactoryServant extends UnicastRemoteObject implements IUserFactory, RMIServant {
-	private static final long serialVersionUID = 1L;
+public class UserFactoryServant implements IUserFactory, RMIServant {
 	private IUserFactoryRMI _userFactory;
 
 	protected UserFactoryServant() throws RemoteException {

931d4ed675caa707f908cfb4023077fa6c4c2de2
DaGrisa <dgr7348@students.fhv.at>
2015-10-29 13:01:13 +0100

    log user rmi
    Signed-off-by: DaGrisa <dgr7348@students.fhv.at>

--- root/communication-rmi/src/main/java/at/fhv/itb5c/rmi/server/UserFactoryRMI.java
+++ root/communication-rmi/src/main/java/at/fhv/itb5c/rmi/server/UserFactoryRMI.java
@@ -9,8 +9,9 @@ import at.fhv.itb5c.application.controller.UserFactoryImpl;
 import at.fhv.itb5c.commons.dto.IUser;
 import at.fhv.itb5c.commons.dto.rmi.IUserFactoryRMI;
 import at.fhv.itb5c.commons.dto.rmi.IUserRMI;
+import at.fhv.itb5c.logging.ILogger;
 
-public class UserFactoryRMI extends UnicastRemoteObject implements IUserFactoryRMI, RMIServant {
+public class UserFactoryRMI extends UnicastRemoteObject implements IUserFactoryRMI, RMIServant, ILogger {
 	private static final long serialVersionUID = 1L;
 	private UserFactoryImpl _factory;
 
@@ -21,17 +22,20 @@ public class UserFactoryRMI extends UnicastRemoteObject implements IUserFactoryR
 
 	@Override
 	public IUserRMI createUser() {
+		log.info("new user requested");
 		IUser user = _factory.createUser();
 		return UserConverterRMI.toRMI(user);
 	}
 
 	@Override
 	public void save(IUser user) {
+		log.info("saving user");
 		_factory.save(UserConverterRMI.toRMI(user));
 	}
 	
 	@Override
 	public void init(String host, int port) throws RemoteException, MalformedURLException {
+		log.info("... initializing UserFactoryRMI");
 		Naming.rebind("rmi://" + host + ":" + port + "/UserFactory", this);
 	}
 }

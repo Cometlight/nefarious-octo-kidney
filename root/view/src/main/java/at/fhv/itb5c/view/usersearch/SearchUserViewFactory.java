a9fbf42eaf3381eed0168c979c45811700025aff
Norskan <simon.angerer@hotmail.com>
2015-10-31 05:47:09 +0100

    implemented search user controller and model
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

--- root/view/src/main/java/at/fhv/itb5c/view/usersearch/SearchUserViewFactory.java
+++ root/view/src/main/java/at/fhv/itb5c/view/usersearch/SearchUserViewFactory.java
@@ -3,6 +3,7 @@ package at.fhv.itb5c.view.usersearch;
 import java.io.IOException;
 import java.rmi.RemoteException;
 import at.fhv.itb5c.view.util.AlertUtil;
+import at.fhv.itb5c.view.util.PanelCloseHandler;
 import at.fhv.itb5c.view.util.RouteProvider;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
@@ -14,7 +15,15 @@ public final class SearchUserViewFactory {
 		FXMLLoader loader = new FXMLLoader();
 
 		loader.setLocation(RouteProvider.getInstance().get(SearchUserController.class));
-
+		SearchUserController searchUserController = new SearchUserController();
+		searchUserController.setPanelCloseHandler(new PanelCloseHandler() {
+			@Override
+			public void close() {
+				paneToPlaceIn.getChildren().clear();
+			}
+		});
+			
+		loader.setController(searchUserController);
 		try {
 
 			paneToPlaceIn.getChildren().add(loader.load());
@@ -23,5 +32,7 @@ public final class SearchUserViewFactory {
 		} catch (RemoteException e1) {
 			AlertUtil.ConnectionAlert();
 		}
+		
+		searchUserController.initialize();
 	}
 }

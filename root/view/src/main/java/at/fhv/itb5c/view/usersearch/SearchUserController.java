f9e16be1f55783f43566de5031fc4c2e02c854fc
Norskan <simon.angerer@hotmail.com>
2015-11-01 16:05:13 +0100

    finished implementation of search user
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

--- root/view/src/main/java/at/fhv/itb5c/view/usersearch/SearchUserController.java
+++ root/view/src/main/java/at/fhv/itb5c/view/usersearch/SearchUserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.List;
 
+import at.fhv.itb5c.commons.dto.rmi.IDepartmentRMI;
 import at.fhv.itb5c.commons.dto.rmi.IUserRMI;
 import at.fhv.itb5c.rmi.client.RMIClient;
 import at.fhv.itb5c.view.user.UserViewController.UserViewState;
@@ -13,6 +14,7 @@ import at.fhv.itb5c.view.util.PanelClosable;
 import at.fhv.itb5c.view.util.PanelCloseHandler;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
@@ -23,168 +25,190 @@ import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
-/*TODO(san7985)
- * 	open detail user view after clicking a row
- */
+public class SearchUserController implements PanelClosable {
 
-public class SearchUserController implements PanelClosable{
-	
-	@FXML private TextField _firstNameTextField;	
-	@FXML private TextField _lastNameTextField;	
-	@FXML private CheckBox _paidCheckBox;
-	@FXML private ComboBox _departmentsCombobox;
-	
-	@FXML private TableView<IUserRMI> _searchResultTableView;
-	@FXML private TableColumn<IUserRMI, String> _firstNameTableColumn;
-	@FXML private TableColumn<IUserRMI, String>  _lastNameTableColumn;
-	@FXML private TableColumn<IUserRMI, String>  _addressTableColumn;
-	@FXML private TableColumn<IUserRMI, String>  _dateOfBirthTableColumn;
-	@FXML private TableColumn<IUserRMI, String>  _membershipFeeTableColumn;
-	
+	@FXML
+	private TextField _firstNameTextField;
+	@FXML
+	private TextField _lastNameTextField;
+	@FXML
+	private CheckBox _paidCheckBox;
+	@FXML
+	private ComboBox<IDepartmentRMI> _departmentsCombobox;
+
+	@FXML
+	private TableView<IUserRMI> _searchResultTableView;
+	@FXML
+	private TableColumn<IUserRMI, String> _firstNameTableColumn;
+	@FXML
+	private TableColumn<IUserRMI, String> _lastNameTableColumn;
+	@FXML
+	private TableColumn<IUserRMI, String> _addressTableColumn;
+	@FXML
+	private TableColumn<IUserRMI, String> _dateOfBirthTableColumn;
+	@FXML
+	private TableColumn<IUserRMI, String> _membershipFeeTableColumn;
+
 	private PanelCloseHandler _panelCloseHandler;
 	private SearchUserModel _searchUserModel;
-
 
 	public SearchUserController() {
 		_searchUserModel = new SearchUserModel();
 	}
-	
+
 	public void initialize() {
 		_firstNameTextField.textProperty().bindBidirectional(_searchUserModel.getFirstName());
 		_lastNameTextField.textProperty().bindBidirectional(_searchUserModel.getLastName());
 		_paidCheckBox.selectedProperty().bindBidirectional(_searchUserModel.getIsPaid());
 		_searchResultTableView.setItems(_searchUserModel.getSearchResult());
-		
-		//TODO(san7985): init _departmentsCombobox when the factory can be accessed	
-		
+
+		try {
+			_departmentsCombobox.setItems(
+					FXCollections.observableList(RMIClient.getRMIClient().getDepartmentFactory().getAllDepartments()));
+		} catch (RemoteException e) {
+			AlertUtil.ConnectionAlert();
+		}
+		_departmentsCombobox.valueProperty().bindBidirectional(_searchUserModel.getDepartment());
+
 		initializeTable();
 	}
-	
+
 	private void showUser(IUserRMI selectedUser) {
 		try {
 			_panelCloseHandler.closeNext(new UserViewFactory(UserViewState.detailState, selectedUser));
 		} catch (IOException e) {
-			//TODO(san7985): critical error handling
+			// TODO(san7985): critical error handling
 		}
 	}
-	
-	public void initializeTable() {	
-		_searchResultTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showUser(newValue));
-		
-		_firstNameTableColumn.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<IUserRMI,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
-				String value;
-				try {
-					if(param.getValue().getFirstName() != null) {
-						value = param.getValue().getFirstName().toString();
-					} else {
-						value = "";
-					}
-				} catch (RemoteException e) {
-					AlertUtil.ConnectionAlert();
-					value = "ERROR";
-				}
-				
-				return new SimpleStringProperty(value);
-			}
-		});
-		
-		_lastNameTableColumn.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<IUserRMI,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
-				String value;
-				try {
-					if(param.getValue().getLastName() != null) {
-						value = param.getValue().getLastName().toString();
-					} else {
-						value = "";
-					}
-				} catch (RemoteException e) {
-					AlertUtil.ConnectionAlert();
-					value = "ERROR";
-				}
-				
-				return new SimpleStringProperty(value);
-			}
-		});
-		
-		_addressTableColumn.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<IUserRMI,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {			
-				String value;
-				try {
-					if(param.getValue().getAddress() != null) {
-						value = param.getValue().getAddress().toString();
-					} else {
-						value = "";
-					}
-				} catch (RemoteException e) {
-					AlertUtil.ConnectionAlert();
-					value = "ERROR";
-				}
-				
-				return new SimpleStringProperty(value);
-			}
-		});
 
-		_dateOfBirthTableColumn.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<IUserRMI,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
-				String value;
-				try {
-					if(param.getValue().getDateOfBirth() != null) {
-						value = param.getValue().getDateOfBirth().toString();
-					} else {
-						value = "";
+	public void initializeTable() {
+		_searchResultTableView.getSelectionModel().selectedItemProperty()
+				.addListener((observable, oldValue, newValue) -> showUser(newValue));
+
+		_firstNameTableColumn.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<IUserRMI, String>, ObservableValue<String>>() {
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
+						String value;
+						try {
+							if (param.getValue().getFirstName() != null) {
+								value = param.getValue().getFirstName().toString();
+							} else {
+								value = "";
+							}
+						} catch (RemoteException e) {
+							AlertUtil.ConnectionAlert();
+							value = "ERROR";
+						}
+
+						return new SimpleStringProperty(value);
 					}
-				} catch (RemoteException e) {
-					AlertUtil.ConnectionAlert();
-					value = "ERROR";
-				}
-				
-				return new SimpleStringProperty(value);
-			}
-		});
-		
-		_membershipFeeTableColumn.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<IUserRMI,String>, ObservableValue<String>>() {
-			
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
-				String value;
-				try {
-					if(param.getValue().getDateOfBirth() != null) {
-						value = Boolean.toString(param.getValue().getMembershipFeePaid());
-					} else {
-						value = "";
+				});
+
+		_lastNameTableColumn.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<IUserRMI, String>, ObservableValue<String>>() {
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
+						String value;
+						try {
+							if (param.getValue().getLastName() != null) {
+								value = param.getValue().getLastName().toString();
+							} else {
+								value = "";
+							}
+						} catch (RemoteException e) {
+							AlertUtil.ConnectionAlert();
+							value = "ERROR";
+						}
+
+						return new SimpleStringProperty(value);
 					}
-				} catch (RemoteException e) {
-					AlertUtil.ConnectionAlert();
-					value = "ERROR";
-				}
-				
-				return new SimpleStringProperty(value);
-			}
-		});
+				});
+
+		_addressTableColumn.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<IUserRMI, String>, ObservableValue<String>>() {
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
+						String value;
+						try {
+							if (param.getValue().getAddress() != null) {
+								value = param.getValue().getAddress().toString();
+							} else {
+								value = "";
+							}
+						} catch (RemoteException e) {
+							AlertUtil.ConnectionAlert();
+							value = "ERROR";
+						}
+
+						return new SimpleStringProperty(value);
+					}
+				});
+
+		_dateOfBirthTableColumn.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<IUserRMI, String>, ObservableValue<String>>() {
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
+						String value;
+						try {
+							if (param.getValue().getDateOfBirth() != null) {
+								value = param.getValue().getDateOfBirth().toString();
+							} else {
+								value = "";
+							}
+						} catch (RemoteException e) {
+							AlertUtil.ConnectionAlert();
+							value = "ERROR";
+						}
+
+						return new SimpleStringProperty(value);
+					}
+				});
+
+		_membershipFeeTableColumn.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<IUserRMI, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<IUserRMI, String> param) {
+						String value;
+						try {
+							if(param.getValue().getMembershipFeePaid()) {
+								value = "Yes";
+							} else {
+								value = "No";
+							}
+						} catch (RemoteException e) {
+							AlertUtil.ConnectionAlert();
+							value = "ERROR";
+						}
+
+						return new SimpleStringProperty(value);
+					}
+				});
 	}
-	
+
 	@FXML
-	public void searchButtonOnReleasedEventHandler(MouseEvent mouseEvent){	
+	public void searchButtonOnReleasedEventHandler(MouseEvent mouseEvent) {
 		try {
-			List<IUserRMI> users = RMIClient.getRMIClient().getUserFactory().findUsers(_searchUserModel.getFirstName().getValue(), 
-																_searchUserModel.getLastName().getValue(), 
-																null, //Todo(san7985): add property for department
-																_searchUserModel.getIsPaid().getValue());
-			
+			List<IUserRMI> users;
+			if (_searchUserModel.getDepartment().get() == null) {
+				users = RMIClient.getRMIClient().getUserFactory().findUsers(_searchUserModel.getFirstName().getValue(),
+						_searchUserModel.getLastName().getValue(), null, _searchUserModel.getIsPaid().getValue());
+			} else {
+				users = RMIClient.getRMIClient().getUserFactory().findUsers(_searchUserModel.getFirstName().getValue(),
+						_searchUserModel.getLastName().getValue(), _searchUserModel.getDepartment().get().getId(),
+						_searchUserModel.getIsPaid().getValue());
+			}
+
 			_searchUserModel.getSearchResult().clear();
 			_searchUserModel.getSearchResult().addAll(users);
 		} catch (RemoteException e) {
 			AlertUtil.ConnectionAlert();
 		}
 	}
-	
+
 	@FXML
-	public void cancelButtonOnMouseReleasedEventHandler(MouseEvent mouseEvent){
+	public void cancelButtonOnMouseReleasedEventHandler(MouseEvent mouseEvent) {
 		_panelCloseHandler.close();
 	}
 

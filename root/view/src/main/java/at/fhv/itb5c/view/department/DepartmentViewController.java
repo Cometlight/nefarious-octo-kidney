9db7ffcc823dec051c905426ccd162c7f2d4c36e
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-04 14:09:02 +0100

    create 'view department' view & controller skeleton

---
+++ root/view/src/main/java/at/fhv/itb5c/view/department/DepartmentViewController.java
@@ -0,0 +1,42 @@
+package at.fhv.itb5c.view.department;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+
+public class DepartmentViewController {
+
+    @FXML
+    private Label _departmentNameLabel;
+
+    @FXML
+    private Label _headLabel;
+
+    @FXML
+    private ListView<?> _teamList;
+
+    @FXML
+    private Button _addTeamButton;
+
+    @FXML
+    private ListView<?> _tournamentList;
+
+    @FXML
+    private Label _typeOfSportLabel;
+
+    @FXML
+    private Button _addTournamentButton;
+
+    @FXML
+    void _onAddTeamButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _onAddTournamentButtonClick(ActionEvent event) {
+
+    }
+
+}

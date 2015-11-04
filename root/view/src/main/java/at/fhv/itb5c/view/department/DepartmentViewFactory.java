aefb97e74a329995e9017bc40281813486ac9b7e
Norskan <simon.angerer@hotmail.com>
2015-11-04 16:41:00 +0100

    listview departments

---
+++ root/view/src/main/java/at/fhv/itb5c/view/department/DepartmentViewFactory.java
@@ -0,0 +1,16 @@
+package at.fhv.itb5c.view.department;
+
+import java.io.IOException;
+
+import at.fhv.itb5c.view.util.interfaces.IViewFactory;
+import javafx.scene.layout.Pane;
+
+public class DepartmentViewFactory implements IViewFactory{
+
+	@Override
+	public void create(Pane paneToPlaceIn) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

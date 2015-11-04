2b91e6dbca152b905288e8cec574bb99a16fab02
Norskan <simon.angerer@hotmail.com>
2015-11-04 15:52:10 +0100

    added login model
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

--- root/view/src/main/java/at/fhv/itb5c/view/login/LoginController.java
+++ root/view/src/main/java/at/fhv/itb5c/view/login/LoginController.java
@@ -8,16 +8,28 @@ import at.fhv.itb5c.rmi.client.RMIClient;
 import at.fhv.itb5c.view.AppState;
 import at.fhv.itb5c.view.util.popup.ErrorPopUp;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class LoginController implements ILogger{
 	
-	@FXML
-	private Button _loginButton;
+	@FXML TextField _usernameInput;
+	@FXML PasswordField _passwordInput;
+	
+	private LoginModel _loginModel;
+	
+	public LoginController() {
+		_loginModel = new LoginModel();
+	}
+	
+	public void initialize() {
+		_usernameInput.textProperty().bindBidirectional(_loginModel.getUserName());
+		_passwordInput.textProperty().bindBidirectional(_loginModel.getPassword());
+	}
 
 	@FXML
-	public void loginButtonOnMouseReleasedButtonHandler(MouseEvent event) {		
+	public void onSignInMouseReleassedHandler(MouseEvent event) {		
 		try {
 			//TODO(san7985) replace with rmi functionality for checking the login
 			IUserRMI _loggedInUser = RMIClient.getRMIClient().getUserFactory().createUser();

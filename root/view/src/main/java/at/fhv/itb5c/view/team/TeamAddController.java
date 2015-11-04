d5bb60e992cf57fbaba66fa9cc8754adfbc22eaa
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-04 13:36:57 +0100

    create 'add team' view & controller skeleton

---
+++ root/view/src/main/java/at/fhv/itb5c/view/team/TeamAddController.java
@@ -0,0 +1,45 @@
+package at.fhv.itb5c.view.team;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+public class TeamAddController {
+
+    @FXML
+    private ListView<?> _userSearchResultList;
+
+    @FXML
+    private Button _cancelButton;
+
+    @FXML
+    private ComboBox<?> _leagueChoice;
+
+    @FXML
+    private TextField _teamNameInput;
+
+    @FXML
+    private TextField _userSearchInput;
+
+    @FXML
+    private Button _saveButton;
+
+    @FXML
+    void _onSearchInputChange(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _onSaveButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void _onCancelButtonClick(ActionEvent event) {
+
+    }
+
+}

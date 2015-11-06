034e7027f0b77ae0520f83b8dc523339ed5f8617
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-11-06 14:27:17 +0100

    Add functionality to existing League classes + search teams by league should work now

--- root/model/src/main/java/at/fhv/itb5c/model/entity/League.java
+++ root/model/src/main/java/at/fhv/itb5c/model/entity/League.java
@@ -1,5 +1,29 @@
 package at.fhv.itb5c.model.entity;
 
-public class League extends PersistableObject {
+import javax.persistence.Column;
 
+import at.fhv.itb5c.commons.enums.TypeOfSport;
+
+public class League extends PersistableObject {
+	@Column(name = "name", nullable = false)
+	private String _name;
+	
+	@Column(name = "typeOfSport", nullable = false)
+	private TypeOfSport _typeOfSport;
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public TypeOfSport getTypeOfSport() {
+		return _typeOfSport;
+	}
+
+	public void setTypeOfSport(TypeOfSport typeOfSport) {
+		_typeOfSport = typeOfSport;
+	}
 }

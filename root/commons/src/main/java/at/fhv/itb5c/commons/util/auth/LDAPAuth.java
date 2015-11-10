fb991dafb9b9eaebdb355ea702340fc5417e5025
pr4chimishra <prachim3193@gmail.com>
2015-11-10 15:47:52 +0100

    ldap login util
    Signed-off-by: pr4chimishra <prachim3193@gmail.com>

---
+++ root/commons/src/main/java/at/fhv/itb5c/commons/util/auth/LDAPAuth.java
@@ -0,0 +1,41 @@
+package at.fhv.itb5c.commons.util.auth;
+
+import java.util.Properties;
+
+import at.fhv.itb5c.commons.property.PropertyManager;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
+public class LDAPAuth {
+	public static String ldapLogin(String username, String password) {
+		Properties env = new Properties();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put(Context.PROVIDER_URL,PropertyManager.getInstance().getProperty("at.fhv.itb5c.commons.util.auth.ldap.host") );
+
+		DirContext ctx;
+		try {
+			ctx = new InitialDirContext(env);
+			SearchControls ctls = new SearchControls();
+			String filter = "(&(uid=" + username + ")(cn=*))";
+			NamingEnumeration<SearchResult> res = ctx.search("ou=fhv, ou=People", filter, ctls);
+			if (res.hasMore()) {
+				res.next();
+				if (!res.hasMore()) {
+					env.put(Context.SECURITY_AUTHENTICATION, "simple");
+					env.put(Context.SECURITY_PRINCIPAL, "uid=" + username + ",ou=fhv,ou=People,dc=uclv,dc=net");
+					env.put(Context.SECURITY_CREDENTIALS, password);
+					new InitialDirContext(env);
+					return username;
+				}
+			}
+		} catch (NamingException e) {
+		}
+		return null;
+	}
+}

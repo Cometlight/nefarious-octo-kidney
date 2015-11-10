b391e0358185ed593edfe8df284e578176c386c0
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-11-10 14:00:55 +0100

    Fix recursive converter

--- root/application/src/main/java/at/fhv/itb5c/application/dto/DepartmentConverter.java
+++ root/application/src/main/java/at/fhv/itb5c/application/dto/DepartmentConverter.java
@@ -1,8 +1,10 @@
 package at.fhv.itb5c.application.dto;
 
 import at.fhv.itb5c.commons.dto.IDepartment;
+import at.fhv.itb5c.commons.dto.IUser;
 import at.fhv.itb5c.logging.ILogger;
 import at.fhv.itb5c.model.entity.Department;
+import at.fhv.itb5c.model.entity.User;
 
 public class DepartmentConverter implements ILogger {
 	public static IDepartment toDTO(Department department){
@@ -17,7 +19,27 @@ public class DepartmentConverter implements ILogger {
 			departmentdto.setVersion(department.getVersion());
 			departmentdto.setName(department.getName());
 			departmentdto.setTypeOfSport(department.getTypeOfSport());
-			departmentdto.setHead(UserConverter.toDTO(department.getHead()));
+			departmentdto.setHead(UserConverter.toDTO(department.getHead(), departmentdto));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		
+		return departmentdto;
+	}
+	
+	public static IDepartment toDTO(Department department, IUser iUser){
+		if(department == null){
+			return null;
+		}
+		
+		IDepartment departmentdto = new DepartmentDTO();
+		
+		try {
+			departmentdto.setId(department.getId());
+			departmentdto.setVersion(department.getVersion());
+			departmentdto.setName(department.getName());
+			departmentdto.setTypeOfSport(department.getTypeOfSport());
+			departmentdto.setHead(iUser);
 		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
@@ -37,7 +59,27 @@ public class DepartmentConverter implements ILogger {
 			department.setVersion(departmentdto.getVersion());
 			department.setName(departmentdto.getName());
 			department.setTypeOfSport(departmentdto.getTypeOfSport());
-			department.setHead(UserConverter.toEntity(departmentdto.getHead()));
+			department.setHead(UserConverter.toEntity(departmentdto.getHead(), department));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		
+		return department;
+	}
+	
+	public static Department toEntity(IDepartment departmentdto, User user){
+		if(departmentdto==null){
+			return null;
+		}
+		
+		Department department = new Department();
+		
+		try {
+			department.setId(departmentdto.getId());
+			department.setVersion(departmentdto.getVersion());
+			department.setName(departmentdto.getName());
+			department.setTypeOfSport(departmentdto.getTypeOfSport());
+			department.setHead(user);
 		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
--- root/application/src/main/java/at/fhv/itb5c/application/dto/UserConverter.java
+++ root/application/src/main/java/at/fhv/itb5c/application/dto/UserConverter.java
@@ -1,11 +1,45 @@
 package at.fhv.itb5c.application.dto;
 
+import at.fhv.itb5c.commons.dto.IDepartment;
 import at.fhv.itb5c.commons.dto.IUser;
 import at.fhv.itb5c.logging.ILogger;
+import at.fhv.itb5c.model.entity.Department;
 import at.fhv.itb5c.model.entity.User;
 
 public class UserConverter implements ILogger {
 	public static IUser toDTO(User user){
+		// null in - null out
+		if(user==null){
+			return null;
+		}
+				
+		// convert
+		IUser userdto = new UserDTO();
+		try {
+			userdto.setId(user.getId()); // id is needed for jpa to identify the object
+			userdto.setVersion(user.getVersion());
+			userdto.setFirstName(user.getFirstName());
+			userdto.setLastName(user.getLastName());
+			userdto.setEmail(user.getEmail());
+			userdto.setTelephoneNumber(user.getTelephoneNumber());
+			userdto.setGender(user.getGender());
+			userdto.setAddress(user.getAddress());
+			userdto.setDateOfBirth(user.getDateOfBirth());
+			userdto.setMembershipFee(user.getMembershipFee());
+			userdto.setRoles(user.getRoles());
+			userdto.setTypeOfSports(user.getTypeOfSports());
+			userdto.setMembershipFeePaid(user.getMembershipFeePaid());
+			userdto.setDepartment(DepartmentConverter.toDTO(user.getDepartment(), userdto));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		
+		log.debug("Converting User Object Model2DTO: " + userdto);
+		
+		return userdto;
+	}
+	
+	public static IUser toDTO(User user, IDepartment iDepartment){
 		// null in - null out
 		if(user==null){
 			return null;
@@ -26,7 +60,7 @@ public class UserConverter implements ILogger {
 		userdto.setRoles(user.getRoles());
 		userdto.setTypeOfSports(user.getTypeOfSports());
 		userdto.setMembershipFeePaid(user.getMembershipFeePaid());
-		userdto.setDepartment(DepartmentConverter.toDTO(user.getDepartment()));
+		userdto.setDepartment(iDepartment);
 		
 		log.debug("Converting User Object Model2DTO: " + userdto);
 		
@@ -56,7 +90,41 @@ public class UserConverter implements ILogger {
 			user.setRoles(userdto.getRoles());
 			user.setTypeOfSports(userdto.getTypeOfSports());
 			user.setMembershipFeePaid(userdto.getMembershipFeePaid());
-			user.setDepartment(DepartmentConverter.toEntity(userdto.getDepartment()));
+			user.setDepartment(DepartmentConverter.toEntity(userdto.getDepartment(), user));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return null;
+		} 
+		
+		log.debug("Converting User Object DTO2Model: " + user);
+		
+		return user;
+	}
+	
+	public static User toEntity(IUser userdto, Department department){
+		// null in - null out
+		if(userdto==null){
+			return null;
+		}
+		
+		// convert
+		User user = new User();
+		try {
+			// id is needed for jpa to identify the object
+			user.setId(userdto.getId());
+			user.setVersion(userdto.getVersion());
+			user.setFirstName(userdto.getFirstName());
+			user.setLastName(userdto.getLastName());
+			user.setEmail(userdto.getEmail());
+			user.setTelephoneNumber(userdto.getTelephoneNumber());
+			user.setGender(userdto.getGender());
+			user.setAddress(userdto.getAddress());
+			user.setDateOfBirth(userdto.getDateOfBirth());
+			user.setMembershipFee(userdto.getMembershipFee());
+			user.setRoles(userdto.getRoles());
+			user.setTypeOfSports(userdto.getTypeOfSports());
+			user.setMembershipFeePaid(userdto.getMembershipFeePaid());
+			user.setDepartment(department);
 		} catch (Exception e) {
 			log.error(e.getMessage());
 			return null;

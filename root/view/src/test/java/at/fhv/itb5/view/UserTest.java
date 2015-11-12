c15f82a7ea155f71430dc657b34a81e23431b879
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-12 11:05:33 +0100

    fix line endings and remove trailing whitespace

--- root/view/src/test/java/at/fhv/itb5/view/UserTest.java
+++ root/view/src/test/java/at/fhv/itb5/view/UserTest.java
@@ -6,23 +6,23 @@ import org.junit.Test;
 import at.fhv.itb5c.rmi.client.RMIClient;
 
 public class UserTest {
-	
+
 	@Before
 	public void before() {
 		//RMIClient.getRMIClient().startUp();
 	}
-	
+
 	@After
 	public void after() {
 		//TODO(san7985): replace when close is implemented
 		//RMIClient.getRMIClient().close();
 	}
-	
+
 	@Test
 	public void createUser() {
 		/*try {
 			IUserRMI user = RMIClient.getRMIClient().getApplicationFacade().createUser();
-			
+
 			if(user == null) {
 				System.out.println("Cant create User!");
 				Assert.assertTrue(false);
@@ -32,12 +32,12 @@ public class UserTest {
 			Assert.assertTrue(false);
 		}*/
 	}
-	
+
 	@Test
 	public void readUserTypesOfSport() {
 		/*try {
 			IUserRMI user = RMIClient.getRMIClient().getApplicationFacade().createUser();
-			
+
 			Set<TypeOfSport> sports = user.getTypeOfSports();
 			if(sports == null) {
 				System.out.println("No sports set!");

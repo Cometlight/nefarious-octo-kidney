51a0c199ae56e2ea3051cc91eb7f0e66d52c7010
DaGrisa <dgr7348@students.fhv.at>
2015-11-20 16:40:59 +0100

    test
    Signed-off-by: DaGrisa <dgr7348@students.fhv.at>

--- root/communication-jms/src/main/java/at/fhv/itb5c/jms/QueueManager.java
+++ root/communication-jms/src/main/java/at/fhv/itb5c/jms/QueueManager.java
@@ -6,6 +6,7 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
@@ -17,48 +18,19 @@ import javax.naming.NamingException;
 import at.fhv.itb5c.logging.ILogger;
 
 public class QueueManager implements ILogger {
+	String def_windows_url = "file:///C:/tmp";
 	ConnectionFactory _cf;
 	String MYCF_LOOKUP_NAME = "cn=myQCF";
 	String MYQUEUE_LOOKUP_NAME = "cn=myQ";
 	Queue _queue;
 	Connection _connection;
-	Session _session;
-	
+
 	public void produce(String textMessage) {
-		Hashtable<String, String> env;
-		Context ctx = null;
 
-		env = new Hashtable<>();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
-		// env.put(Context.PROVIDER_URL, url);
-		
+		Session session = createSession();
+		TextMessage msg;
 		try {
-			// Create the initial context.
-			ctx = new InitialContext(env);
-			_cf = (javax.jms.ConnectionFactory) ctx.lookup(MYCF_LOOKUP_NAME);
-			_queue = (javax.jms.Queue) ctx.lookup(MYQUEUE_LOOKUP_NAME);
-			_connection = _cf.createConnection();
-		} catch (NamingException | JMSException e) {
-			log.error(e.getMessage());
-		}
-		
-		log.info("Connection established!");
-		
-		try {
-			_session = _connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			
-			log.info("Session created!");
-
-			// Create the MessageProducer and MessageConsumer
-			MessageProducer msgProducer = _session.createProducer(_queue);
-
-			log.info("Producer created!");
-			
-			// Tell the provider to start sending messages.
-			_connection.start();
-
-			TextMessage msg = _session.createTextMessage(textMessage);
-			
+			msg = session.createTextMessage(textMessage);
 			log.info("Message created!");
 
 			// rcvMsg = (TextMessage) msgConsumer.receive();
@@ -67,16 +39,71 @@ public class QueueManager implements ILogger {
 
 			// Publish the message
 			log.info("Publishing a message to Queue: " + _queue.getQueueName());
+
+			// Create the MessageProducer and MessageConsumer
+			MessageProducer msgProducer = session.createProducer(_queue);
+
+			log.info("Producer created!");
+
 			msgProducer.send(msg, DeliveryMode.NON_PERSISTENT, 4, 0);
 
 			_connection.close();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+	}
+
+	public String consume() {
+		Session session = createSession();
+		try {
+			MessageConsumer msgConsumer = session.createConsumer(_queue);
+			TextMessage msg = (TextMessage) msgConsumer.receive();
+
+			_connection.close();
+			
+			return msg.getText();
 		} catch (JMSException e) {
 			log.error(e.getMessage());
+			return null;
 		}
 	}
 
-	public TextMessage consume(String consumer) {
-		return null;
+	private Session createSession() {
+		Hashtable<String, String> env;
+		Context ctx = null;
+
+		env = new Hashtable<>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
+		env.put(Context.PROVIDER_URL, def_windows_url);
+
+		try {
+			// Create the initial context.
+			ctx = new InitialContext(env);
+			ConnectionFactory cf = (javax.jms.ConnectionFactory) ctx.lookup(MYCF_LOOKUP_NAME);
+			_queue = (javax.jms.Queue) ctx.lookup(MYQUEUE_LOOKUP_NAME);
+			_connection  = cf.createConnection();
+		} catch (NamingException | JMSException e) {
+			log.error(e.getMessage());
+		}
+
+		log.info("Connection established!");
+
+		Session session = null;
+		
+		try {
+			session = _connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			log.info("Session created!");
+
+			// Tell the provider to start sending messages.
+			_connection.start();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return session;
 	}
 }
--- root/communication-jms/src/main/java/at/fhv/itb5c/jms/TestJMS.java
+++ root/communication-jms/src/main/java/at/fhv/itb5c/jms/TestJMS.java
@@ -4,5 +4,8 @@ public class TestJMS {
 	public static void main(String[] args) {
 		QueueManager qm = new QueueManager();
 		qm.produce("Wow a message!");
+		
+		QueueManager qr = new QueueManager();
+		System.out.println("Message: " + qr.consume());
 	}
 }

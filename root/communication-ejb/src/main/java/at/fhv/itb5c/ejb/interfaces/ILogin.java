6df82968e3409b48fe0b099b1caa86b889f618c8
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-12-02 15:19:16 +0100

    EJB pom problems

--- root/communication-ejb/src/main/java/at/fhv/itb5c/ejb/interfaces/ILogin.java
+++
@@ -1,5 +0,0 @@
-package at.fhv.itb5c.ejb.interfaces;
-
-public interface ILogin {
-	String loginLDAP(String username, String password);
-}
--- root/communication-ejb/src/main/java/at/fhv/itb5c/ejb/interfaces/ILoginRemote.java
+++
@@ -1,8 +0,0 @@
-package at.fhv.itb5c.ejb.interfaces;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface ILoginRemote extends ILogin {
-	
-}

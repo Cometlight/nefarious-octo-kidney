bf8d484dad6aca917b2d400695d4da7548bce7f7
Norskan <simon.angerer@hotmail.com>
2015-12-15 08:37:47 +0100

    tutotial implementation
    Signed-off-by: Norskan <simon.angerer@hotmail.com>

---
+++ root/view-jsf/src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+import javax.ejb.Remote;
+
+@Remote
+public interface Calculator {
+    public double add(double x, double y);
+}
---
+++ root/view-jsf/src/main/java/CalculatorImpl.java
@@ -0,0 +1,9 @@
+import javax.ejb.Stateless;
+
+@Stateless
+public class CalculatorImpl implements Calculator {
+
+    public double add(double x, double y) {
+        return x + y;
+    }
+}
